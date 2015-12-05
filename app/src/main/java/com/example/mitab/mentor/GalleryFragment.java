b0217ab6805d89f5a1d2a373e482993647824a93
mitab86 <mitab86@gmail.com>
2015-12-05 15:51:08 +0545

    Initial commit

---
+++ app/src/main/java/com/example/mitab/mentor/GalleryFragment.java
@@ -0,0 +1,95 @@
+package com.example.mitab.mentor;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link GalleryFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link GalleryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GalleryFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    GridView gv;
+    Context context;
+    ArrayList prgmName;
+    public static String [] prgmNameList={"Let Us C","c++","JAVA","Jsp","Microsoft .Net","Android","PHP","Jquery","JavaScript"};
+    public static int [] prgmImages={R.drawable.sample_0,R.drawable.sample,R.drawable.sample_1,R.drawable.sample_2,R.drawable.sample_3,R.drawable.sample_4,R.drawable.sample_5,R.drawable.sample_6,R.drawable.sample_7};
+
+    private OnFragmentInteractionListener mListener;
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GalleryFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GalleryFragment newInstance(String param1, String param2) {
+        GalleryFragment fragment = new GalleryFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v=  inflater.inflate(R.layout.fragment_gallery, container, false);
+        gv=(GridView) v.findViewById(R.id.gridView1);
+        gv.setAdapter(new CustomAdapter(getActivity(), prgmNameList,prgmImages));
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/example/mitab/mentor/VolleySingleton.java
@@ -0,0 +1,25 @@
+package com.example.mitab.mentor;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Mitab on 12/3/2015.
+ */
+public class VolleySingleton {
+    private static VolleySingleton sInstance=null;
+
+    private VolleySingleton(){
+        mRequestQueue= Volley.newRequestQueue();
+    }
+    public static VolleySingleton getsInstance(){
+        if (sInstance == null)
+        {
+            sInstance=new VolleySingleton();
+        }
+        return sInstance;
+    }
+    public RequestQueue getRequestQueue(){
+        return mRequestQueue;
+    }
+}
