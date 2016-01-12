ae83c55a3376c9cfd6f5e13e22d2f1911d5ea866
mitab86 <mitab86@gmail.com>
2016-01-12 13:48:17 +0545

    RequestQueue

--- app/src/main/java/com/example/mitab/mentor/Movies/Pages/PageFragment.java
+++ app/src/main/java/com/example/mitab/mentor/Movies/Pages/PageFragment.java
@@ -1,13 +1,21 @@
 package com.example.mitab.mentor.Movies.Pages;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.mitab.mentor.R;
 
 
@@ -17,6 +25,7 @@ import com.example.mitab.mentor.R;
 public class PageFragment extends Fragment {
 
     private int mPage;
+
     public static final String ARG_PAGE = "ARG_PAGE";
     public static PageFragment newInstance(int page) {
         Bundle args = new Bundle();
@@ -31,8 +40,24 @@ public class PageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_page, container, false);
-        TextView movie = (TextView) view.findViewById(R.id.movie);
-        movie.setText("Fragment #" + mPage);
+        //TextView movie = (TextView) view.findViewById(R.id.movie);
+        //movie.setText("Fragment #" + mPage);
+        RequestQueue requestQueue=Volley.newRequestQueue(getActivity());
+        StringRequest request=new StringRequest(Request.Method.GET, "http://php.net/", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getActivity(),"RESPONSE"+response, Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(),"ERROR"+error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+        requestQueue.add(request);
         // Inflate the layout for this fragment
         return view;
     }
